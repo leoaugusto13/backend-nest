944677ec9fe951538e6f4668b916c06bae814206
Leonardo Augusto <leo.augustobh13@gmail.com>
2023-05-04 22:49:59 -0300

    Primeiro Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306, 
-      username: 'root',
-      password: '123',
-      database: 'nestjs',
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT, 
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
       entities: [__dirname + '/**/*.entity{.js, .ts}'],
       synchronize: true,
-    }),
+    } as TypeOrmModuleOptions),
   ],
   controllers: [],
   providers: [],
